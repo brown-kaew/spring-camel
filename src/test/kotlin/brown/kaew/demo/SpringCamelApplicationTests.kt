26cba0beaf7847b3e7a678aed27d3ce688427cd5
Kaew Tibkham <kaew.tib@ascendcorp.com>
2022-08-22 00:05:34 +0700

    init spring project

---
+++ src/test/kotlin/brown/kaew/demo/SpringCamelApplicationTests.kt
@@ -0,0 +1,13 @@
+package brown.kaew.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCamelApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
