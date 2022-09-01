a49a2e447254ca3700cd743d8b2456d5cfea1648
Kaew Tibkham <kaew.tib@ascendcorp.com>
2022-09-01 16:59:40 +0700

    add new schema register dependencies

--- spring-schema-register-server/src/main/kotlin/brown/kaew/demo/SpringSchemaRegisterApplication.kt
+++ spring-schema-register-server/src/main/kotlin/brown/kaew/demo/SpringSchemaRegisterApplication.kt
@@ -2,10 +2,12 @@ package brown.kaew.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.schema.registry.EnableSchemaRegistryServer
 
 @SpringBootApplication
-class SpringCamelApplication
+@EnableSchemaRegistryServer
+class SpringSchemaRegisterApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringCamelApplication>(*args)
+	runApplication<SpringSchemaRegisterApplication>(*args)
 }
